1ccf6d3ead087ece5e996939292eaed73775e664
Jiamin Shen <shen_jiamin@comp.nus.edu.sg>
2023-06-21 12:47:45 +0800

    Update ssh-wrapper.py

--- ssh-wrapper.py
+++ ssh-wrapper.py
@@ -2,33 +2,31 @@
 
 import os
 import sys
-from pathlib import Path
 
 
 if __name__ == "__main__":
-    # build args
+    if len(sys.argv) == 1:
+        print("Usage: {} [ssh|scp|sftp|sshfs] [args]".format(sys.argv[0]))
+        sys.exit(1)
+
     prog = "ssh"
     args = []
 
-    if len(sys.argv) > 1 and sys.argv[1] in ("ssh", "scp", "sftp"):
+    if sys.argv[1] in ("ssh", "scp", "sftp", "sshfs"):
         prog = sys.argv[1]
         args = sys.argv[2:]
     else:
         args = sys.argv[1:]
 
-    dest = {
-        "ssh-xacc": "xacchead",
-        "ssh-socks": "ssocks",
-    }[Path(sys.argv[0]).stem]
-
     # build envs
     envs = os.environ.copy()
-    envs["SSH_ASKPASS"] = "/home/shenjm/.local/bin/sshaskpass.py"
-    envs["SSH_ASKPASS_REQUIRE"] = "force"
+    if not envs.get("SSH_ASKPASS"):
+        envs["SSH_ASKPASS"] = "sshaskpass.py"
+    if not envs.get("SSH_ASKPASS_REQUIRE"):
+        envs["SSH_ASKPASS_REQUIRE"] = "force"
     if not envs.get("DISPLAY"):
-        print("DISPLAY not set, set to :0", file=sys.stderr)
         envs["DISPLAY"] = ":0"
 
     # run
-    print(prog, dest, *args)
-    os.execvpe(prog, [prog, dest, *args], envs)
+    print(prog, *args)
+    os.execvpe(prog, [prog, *args], envs)
